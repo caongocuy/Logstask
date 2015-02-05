57ae90e41f5d62918a4800a80aeb8d900b28224a
cao ngoc uy <caongocuy@gmail.com>
2015-02-05 09:04:07 +0700

    command 2 dong

--- forwarder-client.sh
+++ forwarder-client.sh
@@ -33,8 +33,8 @@ echo "### CAU HINH LOGSTASH-FORWARDER ###"
 
 sleep 3
 
-forwarder=/etc/logstash-forwarder
-test -f forwarder.orig || cp $forwarder $forwarder.orig
+#forwarder=/etc/logstash-forwarder
+#test -f forwarder.orig || cp $forwarder $forwarder.orig
 
 cat << EOF > $forwarder
 {
