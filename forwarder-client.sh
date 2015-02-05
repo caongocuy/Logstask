b9efa5aecff46ceccc610966522b2773389f4798
cao ngoc uy <caongocuy@gmail.com>
2015-02-05 08:42:11 +0700

    forwarder-client

---
+++ forwarder-client.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+#### CAU HINH LOGSTASH-FORWARDER TREN CLIENT ####
+
+echo "#### BAT DAU ####"
+
+sleep 3
+
+### ADD repo ###
+echo 'deb http://packages.elasticsearch.org/logstashforwarder/debian stable main' | sudo tee /etc/apt/sources.list.d/logstashforwarder.list
+
+### ADD key de khac phuc loi NO PUB KEY ###
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys D27D666CD88E42B4
+
+### UPDATE he thong ###
+sudo apt-get update
+
+### CAI DAT LOGSTASH-FORWARDER ###
+sudo apt-get install logstash-forwarder
+
+### CAU HINH TU DONG RUN CHO LOGSTASH-FORWARDER ###
+cd /etc/init.d/; sudo wget https://raw.github.com/elasticsearch/logstash-forwarder/master/logstash-forwarder.init -O logstash-forwarder
+
+### CAP QUYEN THUC THI ###
+sudo chmod +x logstash-forwarder
+sudo update-rc.d logstash-forwarder defaults
+
+### TAO THU MUC certs va COPY crt VAO THU MUC VUA TAO ###
+sudo mkdir -p /etc/pki/tls/certs
+sudo cp /tmp/logstash-forwarder.crt /etc/pki/tls/certs/
+
+echo "### CAU HINH LOGSTASH-FORWARDER ###"
+
+sleep 3
+
+forwarder=/etc/logstash-forwarder
+test -f forwarder.orig || cp $forwarder $forwarder.orig
+
+cat << EOF > $forwarder
+{
+  "network": {
+    "servers": [ "10.145.37.106:5000" ],
+    "timeout": 15,
+    "ssl ca": "/etc/pki/tls/certs/logstash-forwarder.crt"
+  },
+  "files": [
+    {
+      "paths": [
+        "/var/log/syslog",
+        "/var/log/auth.log"
+       ],
+      "fields": { "type": "syslog" }
+    }
+   ]
+}
+EOF
+
+echo "### KHOI DONG LAI DICH VU ###"
+sudo service logstash-forwarder restart
+
+sleep 3
+
+echo "### KET THUC QUA TRINH CAI DAT VA CAU HINH ###"
